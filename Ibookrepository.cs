224a896b3bc14dd7270f1b38b095e24665e92dda
Maria Asandei 🫧 <129296166+mariaasandei@users.noreply.github.com>
2026-04-02 19:43:46 +0300

    Define IBookRepository interface with book methods

--- Ibookrepository.cs
+++ Ibookrepository.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using BookLibrary.Models;
 
-namespace BookLibrary
+namespace BookLibrary.Interfaces;
+
+public interface IBookRepository : IRepository<Book>
 {
-    internal class Ibookrepository
-    {
-    }
+    Task<IEnumerable<Book>> GetBooksWithFiltersAsync(
+        string genre = null,
+        string author = null,
+        int? yearFrom = null,
+        int? yearTo = null,
+        bool? isBorrowed = null,
+        string orderBy = "Title",
+        bool ascending = true);
+    Task<IEnumerable<IGrouping<string, Book>>> GetBooksGroupedByGenreAsync();
+    Task<IEnumerable<IGrouping<string, Book>>> GetBooksGroupedByAuthorAsync();
+    Task<Book> GetBookWithLoansAsync(int bookId);
 }
